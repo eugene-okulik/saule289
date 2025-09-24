2da28880fbe9a20a255b1c89c4f860d00dda0db3
Saule289 <saule289@mail.ru>
2025-09-24 20:05:13 +0400

    modified files according to flake8

--- homework/saule_zhankulova/Homework3/hypotenuse_area.py
+++ homework/saule_zhankulova/Homework3/hypotenuse_area.py
@@ -2,7 +2,7 @@ import math
 
 leg1 = int(input())
 leg2 = int(input())
-hypotenuse  = math.sqrt(leg1**2 + leg2**2)
-area = (leg1 + leg2)/2
+hypotenuse = math.sqrt(leg1**2 + leg2**2)
+area = (leg1 + leg2) / 2
 print(hypotenuse)
 print(area)
