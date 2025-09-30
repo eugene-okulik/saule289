8038b3e1ab84254f2aa59b5b7c2feccbbf31e8ca
Saule289 <saule289@mail.ru>
2025-09-30 20:39:36 +0400

    removw whitespace in results2

--- homework/saule_zhankulova/Homework5/result_of_operations.py
+++ homework/saule_zhankulova/Homework5/result_of_operations.py
@@ -3,7 +3,7 @@ colon_index = results.index(':')
 number_index = results[colon_index + 2:]
 print(int(number_index) + 10)
 
-results1 =  "результат операции: 514"
+results1 = "результат операции: 514"
 colon_index = results1.index(':')
 number_index = results1[colon_index + 2:]
 print(int(number_index) + 10)
