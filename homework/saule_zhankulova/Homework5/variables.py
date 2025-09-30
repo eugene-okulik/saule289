5eaea99c7231164073720925ffc47a6d077a4578
Saule289 <saule289@mail.ru>
2025-09-30 20:28:58 +0400

    added_homework5

---
+++ homework/saule_zhankulova/Homework5/variables.py
@@ -0,0 +1,7 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name = person[0]
+last_name = person[1]
+city = person[2]
+phone = person[3]
+country = person[4]
+print(name, last_name, city, phone, country)
