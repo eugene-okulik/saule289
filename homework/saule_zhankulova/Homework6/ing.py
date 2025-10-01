35f9dc560386ed54b1a369408c671aa4daf2eabc
Saule289 <saule289@mail.ru>
2025-10-01 21:28:50 +0400

    fixed whitespace in ing-file

--- homework/saule_zhankulova/Homework6/ing.py
+++ homework/saule_zhankulova/Homework6/ing.py
@@ -2,7 +2,7 @@ text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
         "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
 format_list = text.split()
 text_list = []
-#print(format_list)
+# print(format_list)
 for word in format_list:
     if word.endswith(',') or word.endswith('.'):
         new_word = word[:-1] + 'ing' + word[-1]
