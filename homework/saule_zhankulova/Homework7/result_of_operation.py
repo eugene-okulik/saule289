cc996e93a8bf7a909bae8297483dcc4ae2b74b8b
Saule289 <saule289@mail.ru>
2025-10-09 17:09:30 +0400

    make whitespace after definition of function

--- homework/saule_zhankulova/Homework7/result_of_operation.py
+++ homework/saule_zhankulova/Homework7/result_of_operation.py
@@ -3,10 +3,12 @@ result1 = "результат операции: 514"
 result2 = "результат работы программы: 2"
 result3 = "результат: 2"
 
+
 def result_of_operation(result):
     colon_index = result.index(':')
     number_index = result[colon_index + 2:]
     print(int(number_index) + 10)
+
 
 result_of_operation(result = result0)
 result_of_operation(result = result1)
