48f5bcbede67446160faea037817c601b70db827
Saule289 <saule289@mail.ru>
2025-10-13 17:59:09 +0400

    homework8 done

---
+++ homework/saule_zhankulova/Homework8/salary.py
@@ -0,0 +1,17 @@
+import random
+
+def calculate_bonus():
+
+    salary = int(input("What is your salary? "))
+    solution = [True, False]
+    bonus = random.choice(solution)
+
+    if bonus:
+        change_salary = salary * random.randint(10, 100)
+        print(f"\u2022 {salary}, {bonus}, - '${change_salary}'")
+        print(f'Your salary will be {change_salary}')
+    else:
+        print(f"\u2022 {salary}, {bonus}, - '${salary}'")
+        print(f'Your salary is not changed')
+
+calculate_bonus()
