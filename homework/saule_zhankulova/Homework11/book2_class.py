3b574419c3576035f46ffd40a8fd05d4a54df27a
Saule289 <saule289@mail.ru>
2025-10-22 13:11:12 +0400

    added homework11

---
+++ homework/saule_zhankulova/Homework11/book2_class.py
@@ -0,0 +1,24 @@
+from book_class import Book
+
+class SchoolClass(Book):
+    def __init__(self, material, text, book_name, author, pages, ISBN, reserve, subject, school_class, tasks=False):
+        super().__init__(material, text, book_name, author, pages, ISBN, reserve)
+        self.subject = subject
+        self.school_class = school_class
+        self.tasks = tasks
+
+math = SchoolClass('paper', 'available', 'Simple Maths','Peterson', '200',
+                   '9781234567897',True, 'Mathematics', 11, True)
+literature = SchoolClass('paper', 'available', 'The Great Words','Mikheev', '300',
+                   '9781234567897',False, 'Literature', 7, False)
+physics = SchoolClass('paper', 'available', 'Phisics Basics','Enstein', '2000',
+                   '9781234567897',False, 'Physics', 10, True)
+
+textbooks = [math, literature, physics]
+for book in textbooks:
+    if book.reserve:
+        print(f"Название: {book.book_name}, Автор: {book.author}, страниц: {book.pages}, "
+              f"предмет: {book.subject}, класс: {book.school_class}, зарезервирована")
+    else:
+       print(f"Название: {book.book_name}, Автор: {book.author}, страниц: {book.pages}, "
+             f"предмет: {book.subject}, класс: {book.school_class}")
---
+++ homework/saule_zhankulova/Homework11/book_class.py
@@ -0,0 +1,49 @@
+class Book:
+    def __init__(self, material, text, book_name, author, pages, ISBN, reserve=False):
+        self.material = material
+        self.text = text
+        self.book_name = book_name
+        self.author = author
+        self.pages = pages
+        self.isbn = ISBN
+        self.reserve = reserve
+
+
+class Detective(Book):
+    def __init__(self, material, text, book_name, author, pages, ISBN, reserve, main_character):
+        super().__init__(material, text, book_name, author, pages, ISBN, reserve)
+        self.main_character = main_character
+
+
+class Classic(Book):
+    school_literature: True
+    type = 'classic'
+
+sherlok_holmes = Detective('paper', 'available', 'The Hound of the Baskervilles',
+                           'Arthur Conan Doyle', '100', '9781234567897',
+                           True, 'Sherlock Holmes')
+
+
+poirot = Detective('paper', 'available', 'A Haunting in Venice',
+                           'Agatha Christie', '99', '456789712345',
+                           False, 'Hercule Poirot')
+
+pushkin = Classic('online', 'available', 'Eugene Onegin','Alexander Pushkin',
+                  '120', '9781234567897',False)
+
+lermontov = Classic('online', 'available', 'The Cliff','Michail Lermintov',
+                    '50', '9781234567897',False)
+
+harry_potter = Classic('online', 'available', 'The Harry Potter','Joanne Rowling',
+                    '300', '9781234567897',False)
+
+books = [sherlok_holmes, poirot, pushkin, lermontov, harry_potter]
+
+
+for book in books:
+    if book.reserve:
+        print(f"Название: {book.book_name}, Автор: {book.author}, страниц: {book.pages}, "
+              f"материал: {book.material}, зарезервирована")
+    else:
+       print(f"Название: {book.book_name}, Автор: {book.author}, страниц: {book.pages}, материал: {book.material}")
+
